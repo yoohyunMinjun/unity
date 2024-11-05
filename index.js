1450a2045f77e706cb1429ecd1d813cd8ddde7d9
Roh <unternehmen02191@gmail.com>
2024-11-05 09:19:42 +0900

    FIX:: 라우터 변경

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const userRouter = require('./src/users');
 const app = express();
 app.use(express.json());
 
-app.use('/users', userRouter);
+app.use('/', userRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
